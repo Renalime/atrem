0cdf67fb8e4d1ea6949dd156cb8edf1f2696260c
Milaner <milanerua@gmail.com>
2019-10-11 20:33:52 +0300

    Added a command line arguments parser

--- tests/main.c
+++ tests/main.c
@@ -1,7 +1,64 @@
 #include "functionality_tests.h"
 #include "unit_tests.h"
+#include <stdio.h>
+
+void parse_cmd_options(int argc, char **argv);
+void show_help();
+
+struct program_options{
+	unsigned char func_tests : 1;
+	unsigned char unit_tests : 1;
+	unsigned char verbose_mode : 1;
+	unsigned char show_help : 1;
+	unsigned char error : 1;
+};
+
+static struct program_options po; 
+
+void show_help(){
+	printf("-h: Show help\n"
+	"-f: Print functionality tests\n"
+	"-u: Print unit tests\n"
+	"-v: Verbose mode\n");
+}
+
+void parse_cmd_options(int argc, char **argv){
+	int i = 1;
+	argv++;
+	for (i; i < argc; i++, argv++){
+		while (((++(*argv))[0]) != '\0'){
+			switch ((*argv)[0]){
+			case 'f':
+				po.func_tests = 1;
+				break;
+			case 'u':
+				po.unit_tests = 1;
+				break;
+			case 'v':
+				po.verbose_mode = 1;
+				break;
+			case 'h':
+				po.show_help = 1;
+				show_help();
+				return;
+			default:
+				printf("Unknown command line argument\n");	
+				show_help();
+				po.error = 1;
+				return;	
+			}
+		}
+	}
+}
 
 int main(int argc, char **argv){
+	parse_cmd_options(argc, argv);
+	if (po.error == 1) {
+		return 1;
+	}
+	if (po.show_help == 1) {
+		return 0;
+	}
 	test_functionality();	
 	test_units();
 	return 0;
