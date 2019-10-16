88866916613d5f722c2a61988078b6b24ebc9bc6
Milaner <milanerua@gmail.com>
2019-10-16 20:19:32 +0300

    Added 'test_framework.h': a test framework for unit testing (should I write tests for the framework itself?)

---
+++ tests/test_framework.h
@@ -0,0 +1,13 @@
+#ifndef __TEST_FRAMEWORK_H
+#define __TEST_FRAMEWORK_H
+
+#include <stdio.h>
+
+#define FAIL() printf("Failed in %s() at %d line\n", __func__, __LINE__)
+#define _assert(test) do { if (!(test)) { FAIL(); return 1; } } while(0)
+#define _check(test) do { if (!(test)) { FAIL(); } } while(0)
+#define _verify(test) do { int r = test(); if (r) { return r; } else { test_count++ } } while(0)
+
+extern unsigned int test_count;
+
+#endif
