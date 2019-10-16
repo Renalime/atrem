23a7fccd0f00ad5039976fc15e57fafee0620535
Milaner <milanerua@gmail.com>
2019-10-16 09:16:30 +0300

    Experimenting with unit tests

--- tests/unit_tests.c
+++ tests/unit_tests.c
@@ -1,7 +1,28 @@
 #include "unit_tests.h"
 #include <stdio.h>
+#include "../src/matcher.h"
+
+#define FAIL() printf("Failure in %s() line %d\n", __func__, __LINE__)
+#define SUCCESS() printf("%s(): passed\n", __func__)
+#define _assert(test) do { if (!(test)) { FAIL(); return 1; } else { SUCCESS(); } } while(0)
+#define _check(test) do { if (!(test)) { FAIL(); } else { SUCCESS(); }
+#define _verify(test) do { int r = test(); tests_run++; if (r) return r; } while(0)
+
+static int tests_run = 0;
+
+int test_matchhere_01()
+{
+	_assert(matchhere("aw$", "aw") == 1);
+	return 0;
+}
+
+int all_tests()
+{
+	_verify(test_matchhere_01);
+	return 0;
+}
 
 void test_units(char verbose){
 	verbose_mode = verbose;
-	printf("Unit tests!\n");
+	all_tests();
 }
--- tests/unit_tests.h
+++ tests/unit_tests.h
@@ -1,7 +1,16 @@
 #ifndef __UNIT_TESTS_H
 #define __UNIT_TESTS_H
 
+#define PASSED 0
+#define N_PASSED 1
+
+
+//static const char *test_messagdde_delimiter = "*******************************"
+static const char *pass_message = ": passed.";
+static const char *n_pass_message = ": not passed.";
 static char verbose_mode;
+
+
 
 void test_units(char verbose);
 
