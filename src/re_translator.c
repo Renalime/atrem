1b080ac09b1b65bf95ed34349d97373931410fc4
Milaner <milanerua@gmail.com>
2019-10-29 21:59:54 +0200

    Added handling escape metacharacter and function for generating tokens.

--- src/re_translator.c
+++ src/re_translator.c
@@ -4,8 +4,11 @@
 a_token_list * a_init_list()
 {
 	a_token_list *l = malloc(sizeof(a_token_list));
-	l = malloc(sizeof(a_token_list));
+	if (l == NULL)
+		return NULL;
 	l->head = malloc(sizeof(a_token_node));
+	if (l->head == NULL)
+		return NULL;
 	l->current = l->head;
 	l->head->a_token = NULL;
 	l->head->next = NULL;
@@ -47,9 +50,51 @@ unsigned char a_add_token(a_reg_exp_token *t, a_token_list *l)
 	return A_NO_ERR;
 }
 
+unsigned char a_check_here(char * reg_exp, a_token_list *l)
+{
+	if (*reg_exp == '\0')
+		return A_NO_ERR;
+	if (*reg_exp == '\\')
+		return a_escape_token(reg_exp + 1, l);	
+	return A_NO_ERR;
+}
+
+unsigned char a_escape_token(char *reg_exp, a_token_list *l)
+{
+	a_char_token text;
+	a_reg_exp_token *token;
+	if (*reg_exp == '\0')
+		return A_ZERO_ESCAPE;
+	if (*reg_exp == '\\') {
+		text.a_char = '\\';
+	} else {
+		text.a_char = *reg_exp;
+	}
+	token = a_gen_token(A_ESCAPE, RE_CHAR_TYPE_CHAR, text, 0);
+	if (token == NULL)
+		return A_MEM_ERR;
+	a_add_token(token, l);
+	return a_check_here(reg_exp + 1, l);
+}
+
+a_reg_exp_token * a_gen_token(int q, unsigned char type, a_char_token c_token, unsigned char is_negated)
+{
+	a_reg_exp_token *token = malloc(sizeof(a_reg_exp_token));
+	if (token == NULL)
+		return NULL;
+	token->a_quantifier = q;
+	token->a_char_token_type = type;
+	token->a_text = c_token;
+	token->a_is_negated = is_negated; 
+	return token;
+}
+
 unsigned char a_re_translate(char *reg_exp)
 {
 	a_token_list *l = a_init_list();
+	if (l == NULL)
+		return A_MEM_ERR;
+	unsigned char ret = a_check_here(reg_exp, l);
 	a_rm_list(l);
-	return 0;
+	return ret;
 }
--- src/re_translator.h
+++ src/re_translator.h
@@ -1,7 +1,8 @@
 #ifndef __COMPILATION_H
 #define __COMPILATION_H
 
-#define NULL 0
+#define RE_CHAR_TYPE_STR 0
+#define RE_CHAR_TYPE_CHAR 1
 
 enum a_re_quantifiers{A_CHAR, A_STAR, A_ESCAPE, A_PLUS, A_BRACES};
 enum a_errors{A_NO_ERR, A_ZERO_ESCAPE, A_MEM_ERR};
@@ -35,8 +36,10 @@ a_reg_exp_token * a_get_next_token(a_token_list *l);
 unsigned char a_add_token(a_reg_exp_token *t, a_token_list *l);
 void a_rm_list(a_token_list *l);
 a_token_list * a_init_list();
+a_reg_exp_token * a_gen_token(int q, unsigned char type, a_char_token text, unsigned char is_negated);
 unsigned char a_re_translate(char *reg_exp);
-
+unsigned char a_check_here(char *reg_exp, a_token_list *l);
+unsigned char a_escape_token(char *reg_exp, a_token_list *l);
 
 
 #endif
