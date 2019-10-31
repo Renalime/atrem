3f814c187a65d451976b1382644b67972fd6373f
Milaner <milanerua@gmail.com>
2019-10-31 21:11:51 +0200

    Added a file for global definitions.

---
+++ src/globals.h
@@ -0,0 +1,11 @@
+#ifndef __GLOBALS_H
+#define __GLOBALS_H
+
+#define RE_CHAR_TYPE_STR 0
+#define RE_CHAR_TYPE_CHAR 1
+
+
+enum a_re_quantifiers{A_CHAR, A_STAR, A_ESCAPE, A_PLUS, A_BRACES};
+enum a_errors{A_NO_ERR, A_ZERO_ESCAPE, A_MEM_ERR};
+
+#endif
