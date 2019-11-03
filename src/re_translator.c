c4e5e04b693c3f68f06505f8a5e6d6898946c283
Milaner <milanerua@gmail.com>
2019-11-03 18:49:32 +0200

    Added a check for circumflex at the beggining.

--- src/re_translator.c
+++ src/re_translator.c
@@ -9,18 +9,24 @@
 - Write a function that checks for an appropriate character in the regex.
 */
 
-unsigned char a_check_here(char * reg_exp, a_token_list *l)
+unsigned char a_check_here(char *reg_exp, a_token_list *l)
 {
 	if (*reg_exp == '\0')
 		return A_NO_ERR;
 	if (*reg_exp == '\\')
 		return a_escape_token(reg_exp + 1, l);	
-	if (*reg_exp == '$')
+	if (*reg_exp == '$' && *(reg_exp + 1) == '\0')
 		return a_generic_token(reg_exp, A_DOLLAR, l);
-	if (*reg_exp == '^')
-		return a_generic_token(reg_exp, A_CIR_FLEX, l);
 
 	return A_INVALID_RE;
+}
+
+unsigned char a_check_cir_flex(char *reg_exp, a_token_list *l)
+{
+	if (*reg_exp == '^')
+		return a_generic_token(reg_exp, A_CIR_FLEX, l);
+	else
+		return a_check_here(reg_exp, l);
 }
 
 unsigned char a_escape_token(char *reg_exp, a_token_list *l)
@@ -53,7 +59,12 @@ unsigned char a_generic_token(char *reg_exp, unsigned char token_type, a_token_l
 	if (token == NULL)
 		return A_MEM_ERR;
 	a_add_token(token, l);
-	return A_NO_ERR;
+	return a_check_here(reg_exp + 1, l);
+}
+
+unsigned char a_is_valid_char(char c)
+{
+	return (c != '^' && c != '$' && c != ']' && c != '+' && c != '*') ? 1 : 0;
 }
 
 unsigned char a_re_translate(char *reg_exp)
@@ -62,7 +73,7 @@ unsigned char a_re_translate(char *reg_exp)
 	a_token_list *l = a_init_list();
 	if (l == NULL)
 		return A_MEM_ERR;
-	ret = a_check_here(reg_exp, l);
+	ret = a_check_cir_flex(reg_exp, l);
 	a_rm_list(l);
 	return ret;
 }
--- src/re_translator.h
+++ src/re_translator.h
@@ -5,10 +5,11 @@
 
 unsigned char a_re_translate(char *reg_exp);
 unsigned char a_check_here(char *reg_exp, a_token_list *l);
+unsigned char a_check_cir_flex(char *reg_exp, a_token_list *l);
 unsigned char a_escape_token(char *reg_exp, a_token_list *l);
 unsigned char a_dollar_token(char *reg_exp, a_token_list *l);
 unsigned char a_cir_flex_token(char *reg_exp, a_token_list *l);
 unsigned char a_generic_token(char *reg_exp, unsigned char token_type, a_token_list *l);
-
+unsigned char a_is_valid_char(char c);
 
 #endif
